794b7c6742f57c5a8770b4b023e45f6fc5910849
Dmitriy <mail@zdima.net>
2021-03-04 12:51:22 -0500

    Use new Apple Notification framework for darwin applications built for 11.00 and up.

--- app/app_darwin.m
+++ app/app_darwin.m
@@ -5,6 +5,17 @@ extern void themeChanged();
 
 #import <Foundation/Foundation.h>
 
+#if (__MAC_OS_X_VERSION_MIN_REQUIRED >= __MAC_11_0)
+#import <UserNotifications/UNUserNotificationCenter.h>
+#import <UserNotifications/UNNotificationContent.h>
+#import <UserNotifications/UNNotificationRequest.h>
+
+BOOL bFyneAlertGranted = NO;
+
+#endif
+
+#if (__MAC_OS_X_VERSION_MIN_REQUIRED < __MAC_11_0)
+
 @interface FyneUserNotificationCenterDelegate : NSObject<NSUserNotificationCenterDelegate>
 
 - (BOOL)userNotificationCenter:(NSUserNotificationCenter*)center
@@ -21,6 +32,7 @@ extern void themeChanged();
 }
 
 @end
+#endif // < MACOS_11
 
 void sendNSUserNotification(const char *, const char *);
 
@@ -34,6 +46,7 @@ bool isDarkMode() {
 }
 
 void sendNotification(const char *title, const char *body) {
+#if (__MAC_OS_X_VERSION_MIN_REQUIRED < __MAC_11_0)
     NSUserNotificationCenter *center = [NSUserNotificationCenter defaultUserNotificationCenter];
     if (center.delegate == nil) {
         center.delegate = [[FyneUserNotificationCenterDelegate new] autorelease];
@@ -45,6 +58,29 @@ void sendNotification(const char *title, const char *body) {
     notification.informativeText = [NSString stringWithUTF8String:body];
     notification.identifier = [NSString stringWithFormat:@"%@-fyne-notify-%@", [[NSBundle mainBundle] bundleIdentifier], uuid];
     [center scheduleNotification:notification];
+#else
+    UNUserNotificationCenter* center = [UNUserNotificationCenter currentNotificationCenter];
+    [center requestAuthorizationWithOptions:(UNAuthorizationOptionAlert)
+        completionHandler:^(BOOL granted, NSError * _Nullable error) {
+        bFyneAlertGranted = granted;
+    }];
+
+    if( bFyneAlertGranted )
+    {
+        NSString *uuid = [[NSUUID UUID] UUIDString];
+        UNMutableNotificationContent *content = [[UNMutableNotificationContent alloc] init];
+        content.targetContentIdentifier = [NSString stringWithFormat:@"%@-fyne-notify-%@", [[NSBundle mainBundle] bundleIdentifier], uuid];
+        content.title = [NSString stringWithUTF8String:title];
+        content.body = [NSString stringWithUTF8String:body];
+
+        UNNotificationRequest* request = [[UNNotificationRequest requestWithIdentifier:uuid content:content trigger:nil] autorelease];
+        [center addNotificationRequest:request withCompletionHandler:^(NSError * _Nullable error) {
+           if (error != nil) {
+               NSLog(@"%@", error.localizedDescription);
+           }
+        }];
+    }
+#endif // < MACOS_11
 }
 
 void watchTheme() {
