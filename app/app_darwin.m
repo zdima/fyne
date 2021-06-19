b5fc9aae45bee9f6648a2e3bd5ddf4f43507a6bc
Dmitriy <mail@zdima.net>
2021-06-19 16:33:10 -0400

    keep streamline the changes with #if/else/endif

--- app/app_darwin.m
+++ app/app_darwin.m
@@ -5,15 +5,6 @@ extern void themeChanged();
 
 #import <Foundation/Foundation.h>
 
-#if (__MAC_OS_X_VERSION_MIN_REQUIRED >= __MAC_11_0)
-#import <UserNotifications/UNUserNotificationCenter.h>
-#import <UserNotifications/UNNotificationContent.h>
-#import <UserNotifications/UNNotificationRequest.h>
-
-BOOL bFyneAlertGranted = NO;
-
-#endif
-
 #if (__MAC_OS_X_VERSION_MIN_REQUIRED < __MAC_11_0)
 
 @interface FyneUserNotificationCenterDelegate : NSObject<NSUserNotificationCenterDelegate>
@@ -32,6 +23,15 @@ BOOL bFyneAlertGranted = NO;
 }
 
 @end
+
+#else
+
+#import <UserNotifications/UNUserNotificationCenter.h>
+#import <UserNotifications/UNNotificationContent.h>
+#import <UserNotifications/UNNotificationRequest.h>
+
+BOOL bFyneAlertGranted = NO;
+
 #endif // < MACOS_11
 
 void sendNSUserNotification(const char *, const char *);
